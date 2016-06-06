566848bda281abb46308b3d0dff63a528cac846e
Thomas Hickman <ThomasHickman@users.noreply.github.com>
2016-06-06 14:37:44 +0100

    Add function calls

--- public/index.js
+++ public/index.js
@@ -1,11 +1,41 @@
-function error() {
+/*errors
+success
+*/
+
+type GrahamsStuff = SuccessObject;
+
+interface SuccessObject{
+    success: {
+        message: string
+    }
+    data: {
+        task: string;//Channel to subscribe to
+    }
+}
+
+interface ErrorObject{
+    error: {
+
+    }
+}
+
+interface PusherData{
+    message: {
+        ids: string[]
+    }
+}
+
+function error(){
     alert("YOU HAZ ERRORS");
     debugger;
 }
-function hideLoadingScreen() {
+
+function hideLoadingScreen(){
     document.getElementById("loadingElement").style.display = "none";
 }
-$(function () { return hideLoadingScreen(); });
+
+$(() => hideLoadingScreen());
+
 var animations = [
     "spin",
     "zoom",
@@ -13,48 +43,56 @@ var animations = [
     "invert",
     "blur",
     "rainbow"
-];
-function displayLoadingScreen() {
+]
+
+function displayLoadingScreen(){
     var animationToUse = animations[Math.floor(Math.random() * animations.length)];
     document.getElementById("loadingElement").style.display = "block";
     document.getElementById("loadingTrollFaceScreen").style.animationName = animationToUse;
 }
-var pusher;
-$(function () {
+
+var pusher: _pusher.Pusher;
+$(() => {
     pusher = new Pusher($('meta[name="pusher"]').attr('content'), {
         cluster: "eu"
     });
-});
-function getImages(query) {
+})
+
+function getImages(query: string){
+    displayLoadingScreen()
     $.post({
         url: "/lol",
         data: {
             text: query
         }
-    }).done(function (httpDataStr) {
-        var httpData = JSON.parse(httpDataStr);
-        if (httpData["error"] != undefined) {
+    }).done((httpDataStr: string) => {
+        var httpData = <GrahamsStuff>JSON.parse(httpDataStr);
+
+        if(httpData["error"] != undefined){
             error();
         }
+
         var channel = pusher.subscribe(httpData.data.task);
-        channel.bind("lol", function (pusherData) {
+        channel.bind("lol", (pusherData: PusherData) => {
             var imageCont = document.createElement("div");
-            pusherData.message.ids.forEach(function (id) {
-                var newImage = document.createElement("img");
+            pusherData.message.ids.forEach(id => {
+                let newImage = document.createElement("img");
                 newImage.src = "result/" + id;
                 imageCont.appendChild(newImage);
-            });
+            })
             document.getElementById("downloadedImageOuter").appendChild(imageCont);
-        });
-    }).fail(function (error) {
+            hideLoadingScreen();
+        })
+    }).fail((error) => {
         error();
-    });
+    })
 }
-function postForm() {
-    if (document.getElementById("inputBox").value != "") {
-        getImages(document.getElementById("inputBox").value);
+
+function postForm(){
+    if((<HTMLInputElement>document.getElementById("inputBox")).value != ""){
+        getImages((<HTMLInputElement>document.getElementById("inputBox")).value);
     }
-    else {
+    else{
         alert("Enter a value");
     }
 }

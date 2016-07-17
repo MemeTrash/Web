6f06e339aa461ae65e9b0726b42d69d473582bf9
Graham Campbell <graham@bugsnag.com>
2016-07-17 01:17:28 +0100

    Refactored to use a controller

--- app/AppServiceProvider.php
+++ app/AppServiceProvider.php
@@ -7,8 +7,6 @@ namespace App;
 use App\Generators\CatGenerator;
 use App\Generators\DogeGenerator;
 use Illuminate\Contracts\Container\Container;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -19,16 +17,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Boot the service provider.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
      * Register the service provider.
      *
      * @return void
@@ -36,24 +24,22 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(CatGenerator::class, function (Container $app) {
-            return new CatGenerator($app->config->get('services.meme.cat'), $app->basePath('resources/img'), $app->basePath('public/result'));
+            return new CatGenerator(
+                $app->config->get('services.meme.cat'), 
+                $app->basePath('resources/img'),
+                $app->basePath('public/result')
+            );
         });
 
         $this->app->singleton(DogeGenerator::class, function (Container $app) {
-            return new DogeGenerator($app->config->get('services.meme.doge'), $app->basePath('public/result'));
+            return new DogeGenerator(
+                $app->config->get('services.meme.doge'),
+                $app->basePath('public/result')
+            );
         });
 
-        $this->app->get('/', function () {
-            return view('index');
-        });
+        $this->app->get('/', 'App\Controllers\MainController@index');
 
-        $this->app->post('lol', function (Request $request) {
-            dispatch(new MemeJob(str_random(16), (string) $request->get('text'), (bool) random_int(0, 1)));
-
-            return new JsonResponse([
-                'success' => ['message' => 'Memes are to follow!'],
-                'data'    => ['task' => $task],
-            ], 202);
-        });
+        $this->app->post('lol', 'App\Controllers\MainController@generate');
     }
 }
